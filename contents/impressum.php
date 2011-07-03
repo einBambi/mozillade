4bb467cb8e009cbffd8ea9c84723c277e9068b4e
atopal <atopal+github@redronin.de>
2011-07-03 15:44:09 +0200

    updated address in impressum

--- contents/impressum.php
+++ contents/impressum.php
@@ -1,7 +1,7 @@
             <h1>Impressum</h1>
 			<h2>Verantwortlich für diese Website ist</h2>
 			<p>Abdulkadir Topal<br />
-			Hohe Str. 61<br />
+			Gallenkampstr. 9<br />
 			47051 Duisburg</p>
 			<p><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (keine technische Hilfe)</p>
 			<p>Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf Straf- und Zivilrechtlich relevante Inhalte überprüft habe, übernehme ich ebenfalls keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am 21. Juni 2011 statt.</p>
