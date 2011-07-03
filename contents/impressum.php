81dd040b44a6ad170f42c77fff713f2fa9422c11
atopal <atopal+github@redronin.de>
2011-07-03 16:44:02 +0200

    changed typos, small fixes

--- contents/impressum.php
+++ contents/impressum.php
@@ -4,4 +4,4 @@
 			Gallenkampstr. 9<br />
 			47051 Duisburg</p>
 			<p><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (keine technische Hilfe)</p>
-			<p>Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf Straf- und Zivilrechtlich relevante Inhalte überprüft habe, übernehme ich ebenfalls keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am 21. Juni 2011 statt.</p>
+			<p>Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf straf- und zivilrechtlich relevante Inhalte überprüft habe, übernehme ich keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am 21. Juni 2011 statt.</p>
