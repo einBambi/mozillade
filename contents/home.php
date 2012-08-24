ea909d18aae7c2d0b959e2ae7fc29da98acff629
Jan Bambach <jan.bambach@hotmail.de>
2012-08-24 23:03:23 +0200

    W3C - Teil 1

--- contents/home.php
+++ contents/home.php
@@ -20,7 +20,7 @@ function textVeraendern(){var string = $('#sachen > .string');if(string.next().l
         <li>
             <h3><a href="http://www.camp-firefox.de">
             <img src="/img/logos/firefox.png" width="50px" height="50px" alt="Firefox-Logo" />
-			<h3 style="font-weight:300; font-family:'Open Sans Light',sans-serif; margin-top:-6px; font-size:22px; color:rgb(175,50,50);">Firefox</h3>
+			<h3 class="programme">Firefox</h3>
 			</a></h3>
                 <p id="blah">Firefox ist ein anpassbarer und sicherer Browser mit vielen Erweiterungen, der zur Förderung von Offenheit, Innovation und Chancen im Internet beitragen soll.</p>
       <a class="button" href="http://www.mozilla.com/de/firefox/">Download</a>
@@ -28,28 +28,28 @@ function textVeraendern(){var string = $('#sachen > .string');if(string.next().l
       <li>
       <h3><a  href="http://www.thunderbird-mail.de">
       <img src="/img/logos/thunderbird.png" width="50px" height="50px" alt="Thunderbird-Logo" />
-            <h3 style="font-weight:300; font-family:'Open Sans Light',sans-serif; margin-top:-6px; font-size:22px; color:rgb(175,50,50);">Thunderbird</h3>
+            <h3 class="programme">Thunderbird</h3>
             </a></h3>
                 <p id="blah">Thunderbird, das E-Mail-Pendant zum Browser Firefox, lässt sich mittels zahlreicher Add-ons ganz leicht den eigenen Bedürfnissen anpassen.</p>
             <a class="button" href="http://www.mozilla.com/de/thunderbird/">Download</a>
       </li>
        <li>
        <h3><a href="http://seamonkey.at/"><img src="/img/logos/seamonkey.png" width="50px" height="50px" alt="SeaMonkey-Logo" />
-            <h3 style="font-weight:300; font-family:'Open Sans Light',sans-serif; margin-top:-6px; font-size:22px; color:rgb(175,50,50);">SeaMonkey</h3>
+            <h3 class="programme">SeaMonkey</h3>
             </a></h3>
                 <p id="blah">SeaMonkey ist ein nützliches und vielfäliges Gesamtpaket welches aus Browser, E-Mail, Newsgroups, Webseiten-Editor und Chat besteht.</p>
             <a class="button" href="http://seamonkey.at/download/">Download</a>
       </li>
        <li>
             <h3> <a href="http://www.sunbird-kalender.de"><img src="/img/logos/lightning.png" width="50px" height="50px" alt="Lightning-Logo" />
-            <h3 style="font-weight:300; font-family:'Open Sans Light',sans-serif; margin-top:-6px; font-size:22px; color:rgb(175,50,50);">Lightning</h3>
+            <h3 class="programme">Lightning</h3>
             </a></h3>
                 <p id="blah">Lightning, entwickelt vom Mozilla Kalenderprojekt, erweitert Thunderbird und SeaMonkey um eine umfangreiche Termin- und Aufgabenverwaltung.</p>
               <a class="button" href="http://www.sunbird-kalender.de/downloads.php">Download</a>
         </li>
         <li>
            <h3><a href="http://kompozer-web.de/"><img src="/img/logos/kompozer.png" width="50px" height="50px" alt="Kompozer-Logo" />
-            <h3 style="font-weight:300; font-family:'Open Sans Light',sans-serif; margin-top:-6px; font-size:22px; color:rgb(175,50,50);">KompoZer</h3>
+            <h3 class="programme">KompoZer</h3>
             </a></h3>
                 <p id="blah">KompoZer, der intuitive Webeditor mit grafischer Benutzeroberfläche und Echtzeitvorschau zum Erstellen von Internetseiten.</p>
            <a class="button" href="http://kompozer-web.de/">Download</a>
--- contents/impressum.php
+++ contents/impressum.php
@@ -1,10 +1,10 @@
-            <h2>Impressum - <h4 style="margin-top:-10px; margin-left:162px; color:rgb(72,72,72);">Verantwortlich für diese Website ist:</h4></h2>
+            <h2>Impressum - <h4 class="impressum">Verantwortlich für diese Website ist:</h4></h2>
 			
-			<br><p style="color:rgb(72,72,72);">Abdulkadir Topal<br />
+			<br><p class="impressumc">Abdulkadir Topal<br />
 			Gallenkampstr. 9<br />
 			47051 Duisburg</p>
-			<p style="color:rgb(72,72,72);"><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (<strong>keine technische Hilfe</strong>)</p>
-			<br><p style="color:rgb(72,72,72); background: rgba(255,255,255,0.8); border:1px solid rgb(214,214,214); border-radius:0.25em; box-shadow:0px 2px 4px rgba(0,0,0,0.1); padding:4px;">Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf straf- und zivilrechtlich relevante Inhalte überprüft habe, übernehme ich keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am XX. August 2012 statt.</p>
+			<p class="impressumc"><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (<strong>keine technische Hilfe</strong>)</p>
+			<br><p class="impressumb">Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf straf- und zivilrechtlich relevante Inhalte überprüft habe, übernehme ich keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am XX. August 2012 statt.</p>
 			</section>
 	</div>
 	<br><br><br>
