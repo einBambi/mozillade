708e97b857bc606ef072310e0cf8d219aa98dbbd
Tobbi <tobbi.bugs@googlemail.com>
2012-08-30 21:23:54 +0200

    Weitere W3C-Fixes fuer das Impressum

--- contents/impressum.php
+++ contents/impressum.php
@@ -5,6 +5,3 @@
 			47051 Duisburg</p>
 			<p class="impressumc"><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (<strong>keine technische Hilfe</strong>)</p>
 			<br><p class="impressumb">Obwohl ich sämtliche von mir verlinkten externen Seiten negativ auf straf- und zivilrechtlich relevante Inhalte überprüft habe, übernehme ich keine Haftung für diese fremden Inhalte, da sich diese dynamisch und ohne mein Wissen ändern können. Sobald ich von relevanten Veränderungen Kenntnis erhalte, werde ich die Verknüpfung entfernen. Die letzte Überprüfung der Links fand am XX. August 2012 statt.</p>
-			</section>
-	</div>
-	<br><br><br>
