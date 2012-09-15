656af0c20bcfc5678cd591d26738fac2c169f66d
Jan Bambach <jan.bambach@hotmail.de>
2012-09-15 16:46:19 +0200

    Design-Update und Hinzufügen des Newsletters

--- planet.php
+++ planet.php
@@ -1,5 +1,6 @@
 <h2>Neuigkeiten <a href="http://planet.mozilla.de/atom.xml" title="Atom Feed abonnieren"><img src="img/icons/feed.png" alt="Feed" class="feedicon" /></a></h2>
 <?php require_once("inc/planet.html"); ?>
 <p class="planetlink">&raquo; <a href="http://planet.mozilla.de">planet.mozilla.de</a></p>
-<p style="color:rgb(51,51,51); margin-top:40px; margin-left:4px; font-family:'Open Sans',sans-serif; font-weight:300;">Du hast Lust darauf, bei Mozilla mitzumachen? Dazu musst du kein HTML-Experte sein (oder wissen, was das bedeutet), du musst nur das Internet lieben.</p>
-<p class="planetlink" style="margin-top:8px;">&raquo; <a href="?site=mitmachen">Mehr</a></p>
+<br><h2>Bleib' am Ball</h2>
+<p style="color:rgb(51,51,51); margin-top:8px; margin-left:8px; font-family:'Open Sans',sans-serif; font-weight:300;">Abonniere einfach unseren monatlichen Newsletter, "Firefox & Du".</p>
+<p class="planetlink" style="margin-top:-4px;">&raquo; <a href="https://www.mozilla.org/de/newsletter/">Mehr</a></p>
