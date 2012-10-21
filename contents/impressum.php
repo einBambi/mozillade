223e7903f9f5b6a4215024821016374ab6921020
Jan Bambach <jan.bambach@hotmail.de>
2012-10-21 19:59:18 +0200

    -˜\£”ﬁ˙#”˙#Á˙¸WEıÏP˘∏˘Å¸∏‰„ı¸
    yay, ein ASCII-Feuerwerk! (also, mit sehr viel Fantasie schon… oder?)

---
+++ contents/impressum.php
@@ -0,0 +1,6 @@
+            <h2>Impressum</h2><h4 class="impressum"> - Verantwortlich für diese Website ist:</h4>
+			
+			<br><p class="impressumc">Abdulkadir Topal<br />
+			Gallenkampstr. 9<br />
+			47051 Duisburg</p>
+			<p class="impressumc"><a href="mailto:a.topal@gmail.com">a.topal@gmail.com</a> (<strong>keine technische Hilfe</strong>)</p><br><br><br>
--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Die deutschsprachige Mozilla-Community &bull; <?=($site !='') ? ucwords($site) : 'mozilla.de' ?></title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css?ver=1.1" />
-    <link href="tabzilla.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/css/styles.css?ver=1.1" />
+    <link href="/tabzilla.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="/favicon.ico">
     <link rel="alternate" href="http://planet.mozilla.de/atom.xml" title="Mozilla.de Planet-Feed abonnieren" type="application/atom+xml">
 </head>
 <body id="<?=($site == '') ? 'home' : $site ?>">
@@ -40,22 +40,22 @@
 			'title' => 'Zur Startseite wechseln',
 			'text' => 'Start'),	
 		1 => array( 
-			'href' => '?site=hilfe',
+			'href' => '../hilfe/',
 			'id' => 'hilfe', 
 			'title' => 'Haben Sie Probleme mit Firefox & Co.? Wir helfen Ihnen weiter!',
 			'text' => 'Hilfe'),
 		2 => array( 
-			'href' => '?site=mitmachen', 
+			'href' => '../mitmachen/', 
 			'id' => 'mitmachen',
 			'title' => 'Möglichkeiten, bei Mozilla mitzumachen',
 			'text' => 'Mitmachen!'),	
 		3 => array( 
-			'href' => '?site=kalender', 
+			'href' => '../kalender/', 
 			'id' => 'kalender',
 			'title' => 'Community-Kalender',
 			'text' => 'Events'),		
 		4 => array( 
-			'href' => '?site=ueber', 
+			'href' => '../ueber/', 
 			'id' => 'ueber',
 			'title' => 'Mehr über Mozilla im deutschsprachigen Raum erfahren',
 			'text' => 'Über uns')
@@ -86,8 +86,7 @@
 </div>
 </div>
 <!-- Skripte -->
-
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script type="text/javascript">
  $(document).ready(function()
  {
@@ -96,8 +95,7 @@
  function setupAktivitaeten(){if($('.aktivitaet').length > 1){$('.aktivitaet:first').addClass('string').fadeIn(1000);setInterval('textVeraendern()', 3000);}}
 function textVeraendern(){var string = $('#sachen > .string');if(string.next().length == 0){string.removeClass('string').fadeOut(1000);$('.aktivitaet:first').addClass('string').fadeIn(1000);}else{string.removeClass('string').fadeOut(1000);string.next().addClass('string').fadeIn(1000);}}
 </script>
-    <script type="text/javascript" src="tabzilla.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/tabzilla.js"></script>
         <!--[if lte IE 8]>
         <script src="/js/html5.js" type="text/javascript"></script>
         <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
