4945e17471a3f8188659c2ca0c5c51d2dc1b3e8a
Jan Bambach <jan.bambach@hotmail.de>
2012-10-31 21:48:28 +0100

    Hinzufügen des Thunderbird-Chats, Anordnen der Links auf der Hilfeseite und Verschmelzen der beiden Cronjobs

--- contents/hilfe.php
+++ contents/hilfe.php
@@ -3,34 +3,39 @@
 		0 => array( 
 			'name' => 'Firefox', 
 			'btnlabel' => 'Forum',
-			'url' => 'http://www.camp-firefox.de/',
+			'url' => 'http://www.camp-firefox.de/forum/',
 			'logo' => 'firefox'),
 		1 => array( 
 			'name' => 'Firefox-Chat', 
 			'btnlabel' => 'Betreten',
-			'url' => 'irc://moznet/firefox.de',
+			'url' => 'http://widget00.mibbit.com/?settings=ac94eecf36f16bd1854e7a27ad5d6562&server=irc.mozilla.org&channel=%23firefox.de&nick=',
 			'logo' => 'irc'),
 		2 => array(
 			'name' => 'Thunderbird',
 			'btnlabel' => 'Forum',
-			'url' => 'http://www.thunderbird-mail.de',
+			'url' => 'http://www.thunderbird-mail.de/forum/',
 			'logo' => 'thunderbird'),
 		3 => array( 
+			'name' => 'Thunderbird-Chat', 
+			'btnlabel' => 'Betreten',
+			'url' => 'https://widget00.mibbit.com/?settings=ac94eecf36f16bd1854e7a27ad5d6562&server=irc.mozilla.org&channel=%23thunderbird.de&customprompt=Thunderbird-Chat%20betreten&nick=',
+			'logo' => 'irc'),
+		4 => array( 
 			'name' => 'SeaMonkey', 
-			'btnlabel' => 'Forum',
-			'url' => 'http://www.seamonkey.at/',
+			'btnlabel' => 'Hilfe',
+			'url' => 'http://seamonkey.at/support.html',
 			'logo' => 'seamonkey'),
-		4 => array(
+		5 => array(
 			'name' => 'Lightning',
 			'btnlabel' => 'Forum', 
 			'url' => 'http://www.thunderbird-mail.de/forum/viewforum.php?f=46',
 			'logo' => 'lightning'),
-		5 => array(
+		6 => array(
 			'name' => 'KompoZer',
 			'btnlabel' => 'Forum',
-			'url' => 'http://www.kompozer-web.de/',
+			'url' => 'http://www.kompozer-web.de/forum/',
 			'logo' => 'kompozer'),
-		6 => array(
+		7 => array(
 			'name' => 'Entwickler',
 			'btnlabel' => 'MDN',
 			'url' => 'http://developer.mozilla.org/de/',
@@ -38,7 +43,7 @@
 	);
 ?>
 <h6 class="ml3">Du brauchst Hilfe?</h6>
-<p id="uebertext">Wenn sich Firefox, Thunderbird &amp; Co. nicht richtig benehmen und deshalb langsam sind, abstürzen oder andere Probleme bereiten, helfen wir Dir gerne weiter. Das Einzige, was Du machen musst, ist uns zu kontaktieren - entweder über Foren oder über Chat. Abgesehen von <a href="https://support.mozilla.org/de/home">Support.Mozilla.Org</a> gibt es weitere hilfreiche Seiten:</p>
+<p id="uebertext">Wenn sich Firefox, Thunderbird &amp; Co. nicht richtig benehmen und deshalb langsam sind, abstürzen oder andere Probleme bereiten, helfen wir Dir gerne weiter. Das Einzige, was Du machen musst, ist uns zu kontaktieren - entweder über Foren oder über Chat. Abgesehen von <a href="https://support.mozilla.org/de/home">support.mozilla.org</a> gibt es weitere hilfreiche Seiten:</p>
 <h2>Hier findest Du uns:</h2><br><br>
 <div class="hilfe">
     <table class="t1"><tbody>
